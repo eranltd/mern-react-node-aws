554e6e6358c56a8c2ad66f17ab9b97659d636afa
Eran peled <eranltd@gmail.com>
2020-08-15 11:46:26 +0300

    [EC2]

---
+++ frontend/.next/static/webpack/styles.1c8dc9cd4634858bdfe8.hot-update.js
@@ -0,0 +1,10 @@
+webpackHotUpdate_N_E("styles",{
+
+/***/ "./node_modules/react-quill/dist/quill.bubble.css":
+false,
+
+/***/ 4:
+false
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzdGF0aWMvd2VicGFjay9zdHlsZXMuMWM4ZGM5Y2Q0NjM0ODU4YmRmZTguaG90LXVwZGF0ZS5qcyIsInNvdXJjZVJvb3QiOiIifQ==
